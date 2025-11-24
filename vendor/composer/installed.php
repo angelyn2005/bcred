ca9e93f5a24d6b5cc94bf84f38c58711361c2e71
angelyn2005 <cantosangelyn@gmail.com>
2025-11-24 14:15:15 +0800

    pls

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => '__root__',
-        'pretty_version' => 'dev-master',
-        'version' => 'dev-master',
-        'reference' => '113508b6b953971fa9f194189d7b243c125a2b68',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '2c547972d40556e9383c3e32e666879cdfbab282',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         '__root__' => array(
-            'pretty_version' => 'dev-master',
-            'version' => 'dev-master',
-            'reference' => '113508b6b953971fa9f194189d7b243c125a2b68',
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '2c547972d40556e9383c3e32e666879cdfbab282',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
